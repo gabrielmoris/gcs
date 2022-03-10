cd0e0912add0917068b3d7c693224c51cfd59a67
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-10 16:36:41 +0100

    add style and data to postcard from graphql

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -10,7 +10,7 @@ const PostCard = ({ post }: any) => {
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="object-cover object-center rounded-t-lg absolute h-80 w-full object-top shadow-lg lg:rounded-lg"
+          className="absolute h-80 w-full rounded-t-lg object-cover object-center object-top shadow-lg lg:rounded-lg"
         />
       </div>
       <h1
@@ -29,7 +29,39 @@ const PostCard = ({ post }: any) => {
             width="30px"
             className="rounded-full align-middle"
           />
+          <p className="ml-2 inline align-middle text-lg text-gray-700">
+            {post.author.name}
+          </p>
         </div>
+        <div className="font-medium text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-2 inline h-6 w-6 text-pink-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          <span className="align-middle">
+            {moment(post.createdAt).format('MMM DD, YYYY')}
+          </span>
+        </div>
+      </div>
+      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8 '>
+        {post.excerpt}
+      </p>
+      <div className='text-center'>
+        <Link href={`/post/${post.slug}`}>
+          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-red-700 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+            Continue Reading
+          </span>
+          </Link>
       </div>
     </div>
   )
