f35ef688c4e78f456361c8e83ce10deb5430e54f
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-10 15:47:33 +0100

    postCard in Home working with graphQl

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
-
-const PostCard = ({ post }:any) => {
+const PostCard = ({ post }: any) => {
+  // console.log(post)
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
+      <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="object-cover object-center rounded-t-lg absolute h-80 w-full object-top shadow-lg lg:rounded-lg"
+        />
+      </div>
+      <h1
+        className="mb-8 cursor-pointer text-center text-3xl font-semibold 
+        transition duration-700 hover:text-red-600
+        "
+      >
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
+      </h1>
+      <div className="mb-8 block w-full items-center justify-center text-center lg:flex">
+        <div className="mb-4 mr-8 flex w-full items-center justify-center lg:mb-0 lg:w-auto">
+          <img
+            src={post.author.photo.url}
+            alt={post.author.name}
+            height="30px"
+            width="30px"
+            className="rounded-full align-middle"
+          />
+        </div>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Postcard, Categories, PostWidget } from '../components'
+import { getPost } from '../services'
 
-const posts: { title: string; excerpt: string }[] = [
-  { title: `Title 1`, excerpt: `excerpt one!` },
-  { title: `Title 2`, excerpt: `another excerpt!!!!!` },
-]
-
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}:any) => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -16,8 +12,8 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => {
-            return <Postcard post={post} key={post.title} />
+          {posts.map((post: any) => {
+            return <Postcard post={post.node} key={post.node.title} />
           })}
         </div>
         <div className="col-span-1 lg:col-span-4">
@@ -29,6 +25,14 @@ const Home: NextPage = () => {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPost()) || []
+
+  return{
+    props: {posts}
+  }
 }
 
 export default Home
