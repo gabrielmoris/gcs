397abf9a85db1da2034f5e5dc188e3a5d6503fca
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-11 11:18:46 +0100

    add categories in Header Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Link from 'next/link'
-
-const categories = [
-  { name: 'Category 1', slug: 'category1' },
-  { name: 'Category 2', slug: 'category2' },
-]
+import { getCategories } from '../services'
 
 const Header = () => {
+  const [categories, setCategories]: any = useState([])
+
+  useEffect(() => {
+    getCategories().then((category) => setCategories(category))
+  }, [])
+
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="inline-block w-full border-b border-red-300 py-8">
         <div className="block md:float-left">
           <Link href="/">
-            <span className="cursor-pointer text-4xl font-bold">
-              GCS
-            </span>
+            <span className="cursor-pointer text-4xl font-bold">GCS</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-          {categories.map((category) => {
+          {categories.map((category: any) => {
             return (
               <Link key={category.slug} href={`/category/${category.slug}`}>
-                <span className="md:float-right mt-2 ml-4 cursor-pointer align-middle font-semibold">
+                <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold md:float-right">
                   {category.name}
                 </span>
               </Link>
