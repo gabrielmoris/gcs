7dbefacb8dfeecc87839741d61c836cfce219bf9
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-11 08:46:43 +0100

    add recent/related posts widget at right and graphQL queries

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -6,11 +6,11 @@ const PostCard = ({ post }: any) => {
   // console.log(post)
   return (
     <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
-      <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
+      <div className="relative mx-10 mb-6 overflow-hidden pb-80 shadow-md">
         <img
           src={post.featuredImage.url}
           alt={post.title}
-          className="absolute h-80 w-full rounded-t-lg object-cover object-center object-top shadow-lg lg:rounded-lg"
+          className="absolute h-full w-full rounded-t-lg object-cover object-center shadow-lg lg:rounded-lg"
         />
       </div>
       <h1
@@ -53,15 +53,15 @@ const PostCard = ({ post }: any) => {
           </span>
         </div>
       </div>
-      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8 '>
+      <p className="mb-8 px-4 text-center text-lg font-normal text-gray-700 lg:px-20 ">
         {post.excerpt}
       </p>
-      <div className='text-center'>
+      <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-red-700 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+          <span className="inline-block transform cursor-pointer rounded-full bg-red-700 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:-translate-y-1">
             Continue Reading
           </span>
-          </Link>
+        </Link>
       </div>
     </div>
   )
