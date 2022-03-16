db6cb5f5f1fc77bb081c82c5eba0b77fe96db9c9
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-16 10:22:03 +0100

    bug in comments

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,8 +1,122 @@
 import React from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { submitComment } from '../services'
 
-const CommentsForm = ({slug}:any) => {
+const CommentsForm = ({ slug }: any) => {
+  const [error, setError]: any = useState(false)
+  const [localStorage, setLocalStorage]: any = useState(null)
+  const [showSuccesMessage, setShowSuccesMessage]: any = useState(false)
+  const commentEl: any = useRef()
+  const nameEl: any = useRef()
+  const emailEl: any = useRef()
+  const storeDataEl: any = useRef()
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name')
+    emailEl.current.value = window.localStorage.getItem('email')
+  }, [])
+
+  const handleCommentSubmission = () => {
+    setError(false)
+    const { value: comment } = commentEl.current
+    const { value: name } = nameEl.current
+    const { value: email } = emailEl.current
+    const { checked: storeData } = storeDataEl.current
+
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    }
+
+    if (storeData) {
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
+    } else {
+      window.localStorage.removeItem('name')
+      window.localStorage.removeItem('email')
+    }
+    submitComment(commentObj).then((res) => {
+      setShowSuccesMessage(true)
+      setTimeout(() => {
+        setShowSuccesMessage(false)
+      }, 5000)
+    })
+  }
+
   return (
-    <div>CommentsForm</div>
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Comment the post
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-red-400 p-4 text-white placeholder-white outline-none focus:ring-2 focus:ring-red-600"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2 lg:grid-cols-2">
+        <input
+          type="text"
+          ref={nameEl}
+          className="w-full rounded-lg bg-red-400 px-4 py-2 text-white placeholder-white outline-none focus:ring-2 focus:ring-red-600"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="w-full rounded-lg bg-red-400 px-4 py-2 text-white placeholder-white outline-none focus:ring-2 focus:ring-red-600"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <div>
+          <input
+            ref={storeDataEl}
+            type="checkbox"
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className="ml-2 cursor-pointer text-red-500"
+            htmlFor="storeData"
+          >
+            Save my e-mail and name for the next comments
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs font-bold text-green-600">
+          All fields are required
+        </p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="ease inline-block rounded-full bg-red-800 px-8 py-3 text-lg text-white transition duration-500 hover:-translate-y-1 hover:bg-red-300"
+        >
+          Send Comment
+        </button>
+        {showSuccesMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-red-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
   )
 }
 
--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,13 +1,55 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { GraphQLClient, gql } from 'graphql-request'
+import { NextApiRequest, NextApiResponse } from 'next'
 
+const graphqlAPI: any = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+const graphcmsToken: any = process.env.GRAPHCMS_TOKEN
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function comments(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // console.log({graphcmsToken})
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${graphcmsToken}`,
+    },
+  })
+
+  const query = gql`
+    mutation SendComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      sendComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `
+  try {
+    const result = await graphQLClient.request(query, {
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment,
+      slug: req.body.slug,
+    })
+
+    return res.status(200).send(result)
+  } catch (e:any) {
+    console.log('this is the error', e)
+    return res.status(500).send(e)
+
+  }
 }
